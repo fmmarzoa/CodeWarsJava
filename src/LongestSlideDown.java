25c1e06afe8c006feb20cbb573c0a3ec3b3805f4
Fran Marzoa <fmmarzoa@gmail.com>
2017-06-04 23:27:29 +0200

    I think I got it. Major changes ahead!

--- src/LongestSlideDown.java
+++ src/LongestSlideDown.java
@@ -1,18 +1,13 @@
 import java.util.ArrayList;
-import java.util.Collections;
-import java.util.Comparator;
 import java.util.List;
 
-/**
- * Created by fmmarzoa on 6/4/17.
- */
 public class LongestSlideDown {
     private static class TreeNode {
         int data = 0;
         TreeNode left = null;
         TreeNode right = null;
 
-        public TreeNode(int data) {
+        TreeNode(int data) {
             this.data = data;
         }
 
@@ -21,19 +16,7 @@ public class LongestSlideDown {
             return String.format("%d", data);
         }
 
-        public String drawSubTree(int indent) {
-            StringBuilder rtn = new StringBuilder(String.join("", Collections.nCopies(indent, "-")));
-            rtn.append("+");
-            rtn.append(data);
-            rtn.append("\n");
-            if (left != null)
-                rtn.append(left.drawSubTree(indent+1));
-            if (right != null)
-                rtn.append(right.drawSubTree(indent+1));
-            return rtn.toString();
-        }
-
-        public void genTotals(int parentSum, List<Integer> totals) {
+        void genTotals(int parentSum, List<Integer> totals) {
             if ((left == null) && (right == null)) { // leaf!
                 totals.add(parentSum + data);
             } else {
@@ -46,23 +29,33 @@ public class LongestSlideDown {
             }
         }
 
-        public String drawSubTree() {
-            return drawSubTree(0);
-        }
+/*        int findHighest() {
+            List<Integer> totals = new ArrayList<>();
+            genTotals(0, totals);
+            totals.sort(Comparator.naturalOrder());
+            return totals.get(totals.size() - 1);
+        }*/
 
-    }
-
-    private static void preorder(TreeNode root) {
-        if (root != null) {
-            //Visit the node-Printing the node data
-            System.out.printf("%d ", root.data);
-            preorder(root.left);
-            preorder(root.right);
+        void findHighest(int parentSum, int[] highestPath) {
+            if ((left == null) && (right == null)) { // leaf!
+                int leafTotal = parentSum + data;
+                System.out.format("leaf total: %d\n", leafTotal);
+                if (leafTotal > highestPath[0]) {
+                    highestPath[0] = leafTotal;
+                    System.out.format("****************** NEW HIGHEST TOTAL: %d\n",highestPath[0]);
+                }
+            } else {
+                if (left != null) {
+                    left.findHighest(parentSum + data, highestPath);
+                }
+                if (right != null) {
+                    right.findHighest(parentSum + data, highestPath);
+                }
+            }
         }
     }
 
     public static int longestSlideDown(int[][] pyramid) {
-        // TODO create the fucking tree based on the pyramid
         List<TreeNode> previousRowNodes = new ArrayList<>();
         TreeNode topNode = new TreeNode(pyramid[0][0]);
         previousRowNodes.add(topNode);
@@ -70,29 +63,20 @@ public class LongestSlideDown {
             List<TreeNode> thisRowNodes = new ArrayList<>();
             for (int j = 0; j < pyramid[i].length; j++) {
                 TreeNode currentNode = new TreeNode(pyramid[i][j]);
-//                System.out.println("Current node: " + currentNode);
                 thisRowNodes.add(currentNode);
                 if (j < previousRowNodes.size()) {
                     TreeNode parentNodeLeft = previousRowNodes.get(j);
-//                    System.out.println("\tleft node of:  " + parentNodeLeft);
                     parentNodeLeft.left = currentNode;
                 }
                 if (j > 0) {
                     TreeNode parentNodeRight = previousRowNodes.get(j - 1);
-//                    System.out.println("\tright node of: " + parentNodeRight);
                     parentNodeRight.right = currentNode;
                 }
             }
             previousRowNodes = thisRowNodes;
         }
-        List<Integer> totals = new ArrayList<>();
-        topNode.genTotals(0, totals);
-        totals.sort(Comparator.naturalOrder());
-        System.out.println(totals);
-        //System.out.println(topNode.drawSubTree());
-        //preorder(topNode);
-        // TODO traverse the fucking tree to get all paths' sums
-        // TODO return the highest of those sums
-        return -1;
+        int[] highestPath = new int[1];
+        topNode.findHighest(0, highestPath);
+        return highestPath[0];
     }
 }
